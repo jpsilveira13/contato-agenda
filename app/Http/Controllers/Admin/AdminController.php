14f16e2e01fdc4b79b0112ca1913c613d09ee408
João Paulo Silveira <samotinho@gmail.com>
2018-05-13 17:38:23 -0300

    Criacao dos controller do site e do admin

---
+++ app/Http/Controllers/Admin/AdminController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace contatoagenda\Http\Controllers\admin;
+
+use Illuminate\Http\Request;
+use contatoagenda\Http\Controllers\Controller;
+
+class AdminController extends Controller
+{
+    //
+}
---
+++ app/Http/Controllers/Site/SiteController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace contatoagenda\Http\Controllers\site;
+
+use Illuminate\Http\Request;
+use contatoagenda\Http\Controllers\Controller;
+
+class SiteController extends Controller
+{
+    //
+}
