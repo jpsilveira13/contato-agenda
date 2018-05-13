9a00ebbe88e579e6410bd56580f06d8fd2226e5d
João Paulo Silveira <samotinho@gmail.com>
2018-05-13 17:36:59 -0300

    Criando a autenticacao  do site

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,9 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+//rotas do login, cadastro e trocar senha.
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
